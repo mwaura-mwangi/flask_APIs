a4169e8582b17f92207ffe8fad69dcc5b5a231ed
mwaura-mwangi <fmwauramwangi@gmail.com>
2026-08-31 12:09:40 +0300

    add api testing

--- to_do_list/run.py
+++ to_do_list/run.py
@@ -1,6 +1,13 @@
 from app import create_app
+from flask import redirect
 
 app = create_app()
+
+# catch the root path and send send to API
+
+@app.route("/")
+def index():
+    return redirect("/api/todos")
 
 
 if __name__ == "__main__":
